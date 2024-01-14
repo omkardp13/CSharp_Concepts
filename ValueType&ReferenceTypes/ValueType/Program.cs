04d528ed8791c675649c8927f1f9296efca59312
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-14 20:00:56 +0530

    Add files via upload

---
+++ ValueType&ReferenceTypes/ValueType/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a= 10;
+            int b = a; //b gets copy of value a
+            a = 20;
+           
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.Read();
+        }
+    }
+}
