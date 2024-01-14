5697569429485dd45c4c29e820a4bf8df7555fe6
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-14 20:50:05 +0530

    Add files via upload

---
+++ ValueType&ReferenceTypes/PassByOut/RetunMultipleValues/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int sum, difference;
+            CalculateSumDiffrence(10,5,out sum,out difference);
+            Console.WriteLine("Addition is:" + sum);
+            Console.WriteLine("Substraction is:" + difference);
+            Console.Read();
+        }
+
+        static void CalculateSumDiffrence(int a,int b,out int sum,out int diffrence)
+        {
+            sum = a + b;
+            diffrence = a- b;
+        }
+
+    }
+}
