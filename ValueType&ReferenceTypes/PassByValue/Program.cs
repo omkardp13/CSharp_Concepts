82033482a644bda89d7bbc97defda5b18a484db4
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-14 20:20:29 +0530

    Add files via upload

---
+++ ValueType&ReferenceTypes/PassByValue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pass_by_Value
+{
+    internal class Program
+    {
+        static void Increment(int x)
+        {
+            x++;
+            Console.WriteLine("Inside Increment method: " + x);
+        }
+
+        static void Main(string[] args)
+        {
+            int number = 5;
+            Increment(number);
+            Console.WriteLine("Original value: " + number);
+            Console.Read();
+        }
+    }
+}
