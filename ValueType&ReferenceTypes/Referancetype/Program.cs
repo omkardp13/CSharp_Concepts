f0a58a2e416f9a2d27a1d9406f07d0c6470dad34
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-14 20:09:55 +0530

    Add files via upload

---
+++ ValueType&ReferenceTypes/Referancetype/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Person person1 = new Person() { Name="John" };
+
+            Person person2 = person1; //person2 referances the same object as person1
+            person1.Name = "Onkar";
+
+            Console.WriteLine(person1.Name);
+            Console.WriteLine(person2.Name);
+            Console.Read();
+
+
+        }
+    }
+}
