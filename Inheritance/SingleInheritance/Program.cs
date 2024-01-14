9f61a712e8549ab45b0532816f58b2b18c1c8da9
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-14 21:12:57 +0530

    Add files via upload

---
+++ Inheritance/SingleInheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public class AccountInfo
+        {
+            public string Credit()
+            {
+                return "Balace is created";
+            }
+        }
+        public class  DebitInfo:AccountInfo
+        {
+            public string Debit()
+            {
+                Credit();
+                return "Balance is debited";
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+           DebitInfo debitInfo = new DebitInfo();           
+           debitInfo.Debit();
+           
+
+        }
+
+
+    }
+}
