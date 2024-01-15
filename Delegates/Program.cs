bb68bebc0e7703ff46b2b38da5b28687a8631675
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-15 19:52:27 +0530

    Add files via upload

---
+++ Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void Calculations(int a, int b);
+    internal class Program
+    {
+       public static void Addition(int a,int b)
+        {
+            int result = a + b;
+            Console.WriteLine("Addition Result:"+result);
+        }
+
+        static void Main(string[] args)
+        {
+            Calculations obj = new Calculations(Program.Addition);
+            obj.Invoke(20,10);
+            Console.Read();
+        }
+    }
+}
