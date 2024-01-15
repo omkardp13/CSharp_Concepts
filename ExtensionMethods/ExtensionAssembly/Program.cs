273d8da59d2e6646b10b90caa0c5e4d36a0cf308
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-15 09:12:57 +0530

    Add files via upload

---
+++ ExtensionMethods/ExtensionAssembly/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+   public class Program
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
