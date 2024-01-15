c393ea90beffba8ed2b3a2c2635b0a06e5d57a3d
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-15 08:48:31 +0530

    Add files via upload

---
+++ DesignPattern/FactoryPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        public class Car : IVehicle
+        {
+            public void Drive()
+            {
+                Console.WriteLine("Driving a car.");
+            }
+        }
+
+        public class Motorcycle : IVehicle
+        {
+            public void Drive()
+            {
+                Console.WriteLine("Riding a motorcycle.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            VehicleFactory vehicleFactory = new VehicleFactory();
+
+            IVehicle car = vehicleFactory.CreateVehicle("car");
+            car.Drive(); // Output: Driving a car.
+
+            IVehicle motorcycle = vehicleFactory.CreateVehicle("motorcycle");
+            motorcycle.Drive(); // Output: Riding a motorcycle.
+            Console.Read();
+        }
+    }
+}
---
+++ DesignPattern/FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class VehicleFactory
+    {
+        public IVehicle CreateVehicle(string vehicleType)
+        {
+            IVehicle vehicle = null;
+
+            if (vehicleType.ToLower() == "car")
+            {
+                vehicle = new Program.Car();
+            }
+            else if (vehicleType.ToLower() == "motorcycle")
+            {
+                vehicle = new Program.Motorcycle();
+            }
+
+            return vehicle;
+        }
+    }
+}
