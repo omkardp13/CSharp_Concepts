877e8b0f750ab3e4ff9ede5108866a5e93e44720
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-15 20:09:44 +0530

    Add files via upload

---
+++ Delegates/MulticastDelegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegates
+{
+    public delegate void Calculations(int num1, int num2);
+    internal class Program
+    {
+        public static void Addition(int num1,int num2)
+        {
+            int result=num1 + num2;
+            Console.WriteLine(result);
+        }
+
+        public static void Substraction(int num1,int num2)
+        {
+            int result = num1 - num2;
+            Console.WriteLine(result);
+        }
+        static void Main(string[] args)
+        {
+            Calculations obj = new Calculations(Addition);
+            obj += Substraction;
+            obj(150,100);
+            Console.Read();
+        }
+    }
+}
