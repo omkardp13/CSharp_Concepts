656b5991adc889c479fffdc91e4faa439aeeeaf7
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-01-19 08:11:29 +0530

    Add files via upload

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method();
+            Console.WriteLine("Main Thread");
+            Console.ReadLine();
+        }
+
+        public static async void Method()
+        {
+           await Task.Run(new Action(LongTask));
+            Console.WriteLine("New Thread"); //Wait until the long task finishes
+        }
+
+        public static void LongTask()
+        {
+            Thread.Sleep(20000);
+        }
+    }
+}
