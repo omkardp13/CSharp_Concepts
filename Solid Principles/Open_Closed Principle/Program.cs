dafde1403417c5a13d4714bcb643fa4a66163110
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-03-25 20:45:58 +0530

    Add files via upload

---
+++ Solid Principles/Open_Closed Principle/Program.cs
@@ -0,0 +1,30 @@
+﻿abstract class Shape
+{
+  public abstract double Area();
+}
+
+//concreate implementation of shape for rectangle
+
+class Reactangle:Shape
+{
+   public double width {get;set;}
+   public double height {get;set;}
+
+   public override double Area()
+  {
+      return width*height;
+  }
+}
+
+//concreate implementation of shape for circle
+
+class circle:shape
+{
+   public double radius {get;set;}
+   
+
+   public override double Area()
+  {
+      return Math.pi*radius*radius;
+  }
+}
