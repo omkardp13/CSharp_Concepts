83c591bf81885902939d963034164edf44ac8a67
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-03-26 08:08:42 +0530

    Add files via upload

---
+++ Solid Principles/Liskov Substitution Principle(LSP)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal  class Program
+    {
+      class vehicle
+      {
+            public virtual void Move()
+            {
+                Console.WriteLine("Moving...");
+            }
+      }
+      class car : vehicle
+      {
+            public override void Move() 
+            {
+                Console.WriteLine("Driving...");           
+            }
+      }
+
+      class bicycle:vehicle
+      {
+            public override void Move()
+            {
+                Console.WriteLine("Riding...");
+            }
+      }
+
+        static void MoveAll(vehicle[] vehicle)
+        {
+            foreach (var v in vehicle) 
+            { 
+                v.Move();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+
+            car objcar=new car();
+            bicycle objbicycle=new bicycle();
+            vehicle[] vehicles = { objcar, objbicycle };
+            MoveAll(vehicles);
+            Console.ReadLine();
+        }
+    }
+}
