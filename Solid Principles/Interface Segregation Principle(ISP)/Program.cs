c1a5010ded3b4830ea4350fbbfc115ca43c2c42d
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-03-26 08:26:47 +0530

    Add files via upload

---
+++ Solid Principles/Interface Segregation Principle(ISP)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal  class Program
+    {
+     
+        interface IPrint
+        {
+            void Print();
+        }
+     interface IScanner
+        {
+            void Scan();
+        }
+
+        class Photocopyer : IPrint, IScanner
+        {
+            public void Print()
+            {
+                Console.WriteLine("Printing...");
+            }
+
+            public void Scan()
+            {
+                Console.WriteLine("Scanning...");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Photocopyer obj = new Photocopyer();
+            obj.Print();
+            obj.Scan();
+            Console.ReadKey();
+        }
+    }
+}
