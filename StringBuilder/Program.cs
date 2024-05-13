597d68f863eecce6f0622ee1f996e64a46b4fab0
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-05-13 22:13:26 +0530

    Add files via upload

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a new StringBuilder instance
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // Append strings to the StringBuilder
+        stringBuilder.Append("Hello");
+        stringBuilder.Append(" ");
+        stringBuilder.Append("world");
+
+        // Append a newline character
+        stringBuilder.AppendLine();
+
+        // Append formatted string
+        int number = 10;
+        stringBuilder.AppendFormat("The number is {0}.", number);
+
+        // Get the final string
+    //    string finalString = stringBuilder.ToString();
+
+        // Print the final string
+        Console.WriteLine(stringBuilder);
+        Console.ReadLine();
+    }
+}
