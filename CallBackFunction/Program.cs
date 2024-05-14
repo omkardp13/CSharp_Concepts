8e1fbb13a85661f9dc94f2e6fcac58f7eaed11b2
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-05-14 19:35:19 +0530

    Add files via upload

---
+++ CallBackFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    public static void MyCallbackFunction(string message)
+    {
+        Console.WriteLine("Callback received with message: " + message);
+    }
+
+    public static void ProcessData(Action<string> callback)
+    {
+        string processedData = "Hello from ProcessData";
+        callback(processedData);
+    }
+
+    public static void StartProcessing()
+    {
+        ProcessData(MyCallbackFunction);
+    }
+
+
+    static void Main()
+    {
+        StartProcessing();
+        Console.ReadKey();
+    }
+}
