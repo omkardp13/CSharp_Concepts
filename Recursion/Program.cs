b56959f03e7de72f0f7b539ec4139e070d0cce35
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-10-03 20:18:09 +0530

    Add files via upload

---
+++ Recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            fun(x);
+            Console.Read();
+        }
+
+        static void fun(int n)
+        {
+            if(n>0)
+            {
+                Console.WriteLine(n);
+                fun(n-1);
+            }
+        }
+    }
+}
