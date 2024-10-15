e87f39ecdbf9828b49fe6474e0d58d8b26689767
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-10-15 14:48:13 +0530

    Add files via upload

---
+++ yieldKeyword/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompileTimePolymorphism
+{
+    class Program
+    {
+        public static IEnumerable<int> GetNumbers(int upto)
+        {
+            for(int i=0;i<upto;i++)
+            {
+                yield return i;
+                Console.WriteLine("print");
+            }
+        }
+        public static void Main(string[] args) 
+        {
+
+          IEnumerable<int> numbers = GetNumbers(10);
+
+          foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+
+            }
+            Console.Read();
+
+        }
+    }
+}
