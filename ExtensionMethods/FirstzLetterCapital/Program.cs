6660885c8134687cc00b58f4742900951ed05030
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-10-18 11:08:06 +0530

    Add files via upload

---
+++ ExtensionMethods/FirstzLetterCapital/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StringExtensions
+{
+    public static class StringExtensionMethods
+    {
+        // Extension method to get the first character of the string
+        public static char GETFirstAlphabate(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentException("String is null or empty.");
+            }
+
+            return str[0];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "DEMO";
+            string z = "ABC";
+
+            Console.WriteLine(z.GETFirstAlphabate()); // Output: 'A'
+            Console.WriteLine(s.GETFirstAlphabate()); // Output: 'D'
+            Console.Read();
+        }
+    }
+}
