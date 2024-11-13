69df54251bb160eebbc9db1dc026e05993b1eea9
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-13 11:22:30 +0530

    Add files via upload

---
+++ IEnumerable&IQueryable/IQueryable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Threading.Tasks;
+
+class Program
+{
+
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public async Task FilterWithIQueryable()
+    {
+        using (var context = new ProductContext())
+        {
+            IQueryable<Product> expensiveProducts = context.Products.Where(p => p.Price > 500);
+
+            // SQL query executed when enumerating or calling ToListAsync
+            var result = await expensiveProducts.ToListAsync();
+
+            foreach (var product in result)
+            {
+                Console.WriteLine($"{product.Name} - ${product.Price}");
+            }
+        }
+    }
+
+
+    static void Main()
+    {
+
+        FilterWithIQueryable();
+
+
+    }
+}
