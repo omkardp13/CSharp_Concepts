d2f42b876f40ddbc7d867f85c57ae8f4355446f1
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-13 11:01:11 +0530

    Add files via upload

---
+++ IEnumerable&IQueryable/IEnumerable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public static void FilterWithIEnumerable()
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 1200 },
+             new Product { Id = 2, Name = "Tablet", Price = 300 },
+            new Product { Id = 3, Name = "Phone", Price = 700 }
+        };
+
+        IEnumerable<Product> expensiveProducts = products.Where(p => p.Price > 500);
+
+        foreach (var product in expensiveProducts)
+        {
+            Console.WriteLine($"{product.Name} - ${product.Price}");
+        }
+    }
+    static void Main()
+    {
+        FilterWithIEnumerable();
+        Console.Read();
+    }
+
+    
+}
