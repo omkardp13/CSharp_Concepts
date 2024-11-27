de1e257b1c5ff4c3e60fb96ce30ee6a49d2c5fa6
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2024-11-27 11:06:19 +0530

    Add files via upload

---
+++ Constructor/ConstructorCallingSequence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+   
+    public class A
+    {
+        public A()
+        {
+            Console.WriteLine("Constructor of A");
+        }
+    }
+    public class B:A
+    {
+        public B() //Calls the constructor A
+        {
+            Console.WriteLine("Constructor of B");
+        }
+    }
+    public class C:B
+    {
+        public C()
+        {
+            Console.WriteLine("Constructor of C");
+        }
+    }
+
+    static void Main()
+    {
+        C obj = new C();
+        Console.ReadLine();  //OP: Constructor of A
+                             //       Constructor of B
+                             //     Constructor of C
+    }
+}
