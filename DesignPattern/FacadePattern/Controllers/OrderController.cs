ec5ca44cf39739a35d3013b389c396ffa2d544c8
ONKAR PATIL <56990422+omkardp13@users.noreply.github.com>
2025-01-14 12:47:20 +0530

    Add files via upload

---
+++ DesignPattern/FacadePattern/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FacadeDesignPatter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderFacade _orderFacade;
+
+        public OrderController(OrderFacade orderFacade)
+        {
+            _orderFacade = orderFacade;
+        }
+
+        [HttpPost("place")]
+        public IActionResult PlaceOrder([FromBody] OrderRequest request)
+        {
+            var result = _orderFacade.PlaceOrder(request.Username, request.Password, request.OrderId, request.Amount);
+            return Ok(result);
+        }
+    }
+
+    public class OrderRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int OrderId { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+}
